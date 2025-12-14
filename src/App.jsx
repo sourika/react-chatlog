fc07161cf65322488dd81be97ad17c84fcc8a20f
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-12-14 00:09:32 -0800

    refactor App component to remove ChatEntry and directly render ChatLog with messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import './App.css';
-import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 import messages from './data/messages.json';
 
 const App = () => {
-  const firstMessage = messages[0];
   return (
     <div id="App">
       <header>
         <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        <ChatEntry
-          sender={firstMessage.sender}
-          body={firstMessage.body}
-          timeStamp={firstMessage.timeStamp}
-        />
+        <ChatLog entries={messages} />
       </main>
     </div>
   );
