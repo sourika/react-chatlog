1984b7ef2e98a259ea786a0f64ff711a71fc4d88
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-12-13 23:54:00 -0800

    update App component to display chat title and render first message

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import messages from './data/messages.json';
 
 const App = () => {
+  const firstMessage = messages[0];
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={firstMessage.sender}
+          body={firstMessage.body}
+          timeStamp={firstMessage.timeStamp}
+        />
       </main>
     </div>
   );
